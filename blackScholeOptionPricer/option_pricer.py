7ec05590eac361d6c4abfebe108907b106f8a143
ollieellis <ollieellis1@outlook.com>
2024-10-27 14:09:38 +0000

    euro option call pricer

--- blackScholeOptionPricer/option_pricer.py
+++ blackScholeOptionPricer/option_pricer.py
@@ -1,70 +1,91 @@
-#lack of libries to learn from first principles
-from typing import Optional, List, Dict
-from scipy.stats import norm
+# lack of libries to learn from first principles
 from datetime import datetime
+from typing import Dict, List, Optional
+
 import numpy as np
+from scipy.stats import norm
+
 
 class OptionPricer:
-
 
     def __init__(self):
         pass
 
-    def euro_call_pricer(self, risk_free_rate: float, volatilities: np.array, strike_prices: np.array, expiration_date: datetime, current_price: float):
+    def euro_call_pricer(
+        self,
+        risk_free_rate: float,
+        volatilities: np.array,
+        strike_prices: np.array,
+        expiration_date: datetime,
+        current_price: float,
+    ) -> np.array:
         """
-        returns a matrix of option prices for all volality strike price combinations given
-
-        No dividends are paid out during the life of the option.
-        There are no transaction costs in buying the option.
-        The returns of the underlying asset are normally distributed.
-        Volitility (and risk free rate) will be assumed Constant
+        Using the black Scholes model; returns a matrix of option prices for all volality strike price combinations given.
+        c = SN(d1) - N(d2)Ke^-rt
+        d1 = ( ln(s/k) + t(r +0.5*v**2)) / ( v*t^0.5)
+        d2 = d1 - v*t^0.5
         """
-        #c = SN(d1) - N(d2)Ke^-rt
-        #d1 = ( ln(s/k) + t(r +0.5*v**2)) / ( v*t^0.5)
-        #d2 = d1 - v*t^0.5
         time_to_maturity = self.get_time_to_maturity(expiration_date, "years")
-        # vol_time_sqrd = self.get_vol_time_sqrd(volatilities, time_to_maturity, len(strike_prices))
-        d1 = self.get_euro_d1_numerator(risk_free_rate, volatilities, strike_prices, current_price, time_to_maturity)
-        # d1 = np.dot(d1, 1/vol_time_sqrd)
-        print(d1)
-        expected_return = norm.cdf(d1)
-        print(expected_return)
-        # d2 = d1 - vol_time_sqrd
+        d1 = self.get_euro_d1(risk_free_rate, volatilities, strike_prices, current_price, time_to_maturity)
+        d2 = d1 - self.get_euro_d1_denominator(volatilities, time_to_maturity, len(strike_prices))
+        strikes_exp = self.get_strikes_exponent(risk_free_rate, volatilities, strike_prices, time_to_maturity)
+        return current_price * norm.cdf(d1) - norm.cdf(d2) * strikes_exp
 
-    def get_euro_drift_term(self, 
-            risk_free_rate: float, 
-            volatilities: np.array,
-            strike_prices: np.array,
-            current_price: float,
-            years_to_maturity=float,
-        ):
-        #this may need to get renamed if d1 differs in puts ect
-        d1_numerater = self.get_euro_d1_numerator(risk_free_rate, volatilities, strike_prices, current_price, years_to_maturity)
-        # d1_denominator = 
+    def get_euro_d1(
+        self,
+        risk_free_rate: float,
+        volatilities: np.array,
+        strike_prices: np.array,
+        current_price: float,
+        years_to_maturity=float,
+    ) -> np.array:
+        # this may need to get renamed if d1 differs in puts ect
+        d1_numerater = self.get_euro_d1_numerator(
+            risk_free_rate,
+            volatilities,
+            strike_prices,
+            current_price,
+            years_to_maturity,
+        )
+        d1_denominator = self.get_euro_d1_denominator(volatilities, years_to_maturity, len(strike_prices))
+        return d1_numerater / d1_denominator
 
-    def get_euro_d1_numerator(self, 
-            risk_free_rate: float, 
-            volatilities: np.array,
-            strike_prices: np.array,
-            current_price: float,
-            years_to_maturity=float,
-        ):
-        #this may need to get renamed if d1 differs in puts ect
-        ratio_m = current_price/self.get_strikes_matrix(strike_prices, len(volatilities))
-        ln_ratio_m = np.log(ratio_m)
+    def get_strikes_exponent(
+        self,
+        risk_free_rate: float,
+        volatilities: np.array,
+        strike_prices: np.array,
+        years_to_maturity=float,
+    ) -> np.array:
+        return self.get_strikes_matrix(strike_prices, len(volatilities)) * np.exp(-risk_free_rate * years_to_maturity)
 
+    def get_euro_d1_numerator(
+        self,
+        risk_free_rate: float,
+        volatilities: np.array,
+        strike_prices: np.array,
+        current_price: float,
+        years_to_maturity=float,
+        # this may need to get renamed if d1 differs in puts ect
+    ) -> np.array:
+        ratio_m = current_price / self.get_strikes_matrix(strike_prices, len(volatilities))
         v_matrix = self.get_vol_matrix(volatilities, len(strike_prices))
-        time_risk_volatility_matrix = years_to_maturity*(risk_free_rate + 0.5*np.power(v_matrix, 2))
-        # time_risk_volatility_matrix = np.tile(time_risk_volatility_matrix, (len(strike_prices), 1)).transpose()
-        
-        return ln_ratio_m + time_risk_volatility_matrix
-    
+        time_risk_volatility_matrix = years_to_maturity * (risk_free_rate + 0.5 * np.power(v_matrix, 2))
+        return np.log(ratio_m) + time_risk_volatility_matrix
+
+    def get_euro_d1_denominator(
+        self,
+        volatilities: np.array,
+        years_to_maturity: float,
+        num_strikes: int,
+    ):
+        return self.get_vol_matrix(volatilities, num_strikes) * (years_to_maturity**0.5)
+
     def get_vol_matrix(self, volatilities: np.array, num_strikes: int):
         return np.tile(volatilities, (num_strikes, 1)).transpose()
-    
+
     def get_strikes_matrix(self, strike_prices: np.array, num_volatilities: int):
         return np.tile(strike_prices, (num_volatilities, 1))
-
 
     def get_time_to_maturity(self, expiration_date: datetime, time_unit: str):
         if time_unit.lower() != "years":
--- blackScholeOptionPricer/test_option_pricer.py
+++ blackScholeOptionPricer/test_option_pricer.py
@@ -1,11 +1,41 @@
+from datetime import datetime, timedelta
+
 import numpy as np
 from scipy.stats import norm
-from datetime import datetime, timedelta
 
 from option_pricer import OptionPricer
 
 
 class TestEuroCall:
+
+    def test_get_euro_call(self):
+        pricer = OptionPricer()
+        expected = self.get_expected_matrix(self.euro_call)
+        print(expected)
+        params = self.simple_call_params()
+        params.pop("years_to_maturity")
+        params["expiration_date"] = datetime.now() + timedelta(seconds=365.25 * 24 * 60 * 60)
+        result = pricer.euro_call_pricer(**params)
+        print(result)
+        assert abs(np.max(result - expected)) < 0.01
+
+    def test_get_euro_d1(self):
+        pricer = OptionPricer()
+        expected = self.get_expected_matrix(self.euro_call_d1)
+        print(expected)
+        result = pricer.get_euro_d1(**self.simple_call_params())
+        print(result)
+        assert abs(np.max(result - expected)) < 0.01
+
+    def test_strikes_exponent(self):
+        pricer = OptionPricer()
+        expected = self.get_expected_matrix(self.euro_call_strikes_expo)
+        print(expected)
+        params = self.simple_call_params()
+        params.pop("current_price")
+        result = pricer.get_strikes_exponent(**params)
+        print(result)
+        assert abs(np.max(result - expected)) < 0.01
 
     def test_d1_numerator_simple(self):
         pricer = OptionPricer()
@@ -15,30 +45,36 @@ class TestEuroCall:
         print(result)
         assert abs(np.max(result - expected)) < 0.01
 
-    def test_euro_call_pricer_simple(self):
+    def test_d1_denominator_simple(self):
         pricer = OptionPricer()
-        expected = None
-        expiration_date = datetime.now() + timedelta(seconds=365.25*24*60*60)
-        result = pricer.euro_call_pricer(
-            risk_free_rate=1.04, 
-            volatilities=np.array([0.1, 0.2, 0.3]),
-            strike_prices=np.array([100, 105, 110, 120]),
-            current_price=95,
-            expiration_date=expiration_date
+        expected = self.get_expected_matrix(self.euro_call_d1_denominator)
+        print(expected)
+        params = self.simple_call_params()
+        result = pricer.get_euro_d1_denominator(
+            params["volatilities"],
+            params["years_to_maturity"],
+            len(params["strike_prices"]),
         )
+        print(result)
+        assert abs(np.max(result - expected)) < 0.01
+
+    def test_get_euro_d1_denominator_single(self):
+        pricer = OptionPricer()
+        expected = self.euro_call_d1_denominator(95, 100, 1, 1.04, 0.1)
+        result = pricer.get_euro_d1_denominator(np.array([0.1]), 1, 1)
         print(result)
 
     def test_get_strikes_matrix(self):
         vols = 5
-        strikes = np.arange(1,4)
+        strikes = np.arange(1, 4)
         pricer = OptionPricer()
-        assert pricer.get_strikes_matrix(strikes, vols).shape == (5,3)
+        assert pricer.get_strikes_matrix(strikes, vols).shape == (5, 3)
 
     def test_get_vol_matrix(self):
         strikes = 3
-        vols = np.arange(1,6)
+        vols = np.arange(1, 6)
         pricer = OptionPricer()
-        assert pricer.get_vol_matrix(vols, strikes).shape == (5,3)
+        assert pricer.get_vol_matrix(vols, strikes).shape == (5, 3)
 
     def get_expected_matrix(self, desired_matrix_func):
         simple_params = self.simple_call_params()
@@ -50,28 +86,35 @@ class TestEuroCall:
                     K=strike,
                     T=simple_params["years_to_maturity"],
                     r=simple_params["risk_free_rate"],
-                    sigma=v
+                    sigma=v,
                 )
                 expected[i][j] = call_price
         return expected
 
     def euro_call_d1_numerator(self, S, K, T, r, sigma):
-        d1 = (np.log(S/K) + (r + sigma**2/2)*T)
+        d1 = np.log(S / K) + (r + sigma**2 / 2) * T
         return d1
-    
+
+    def euro_call_d1_denominator(self, S, K, T, r, sigma):
+        return sigma * np.sqrt(T)
+
     def euro_call_d1(self, S, K, T, r, sigma):
-        d1 = (np.log(S/K) + (r + sigma**2/2)*T)/ (sigma*np.sqrt(T))
+        d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
         return d1
-        
+
     def euro_call(self, S, K, T, r, sigma):
         N = norm.cdf
-        d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
+        d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
         d2 = d1 - sigma * np.sqrt(T)
-        return S * N(d1) - K * np.exp(-r*T)* N(d2)
-    
+        print(d1)
+        return S * N(d1) - K * np.exp(-r * T) * N(d2)
+
+    def euro_call_strikes_expo(self, S, K, T, r, sigma):
+        return K * np.exp(-r * T)
+
     def simple_call_params(self):
-        return { 
-            "risk_free_rate": 1.04, 
+        return {
+            "risk_free_rate": 1.04,
             "volatilities": np.array([0.1, 0.2, 0.3]),
             "strike_prices": np.array([100, 105, 110, 120]),
             "current_price": 95,
